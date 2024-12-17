236d57b1d545553a22663786cbcc1b2e66da47cb
Diyana Sherin <diya34909@gmail.com>
2024-12-17 21:44:03 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express();
+const env = require("dotenv").config()
+const mongoose = require('mongoose')
+const path = require('path');
+
+
+
+
+const connectDB = mongoose.connect(process.env.MONGO_URI);
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//view engine setup
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'views'))
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+
+//localhost setup
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on localhost:1000`)
+}
+)
+
+
+module.exports = app;
